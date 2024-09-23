f448331e5b5dbb65c3ebc860c7809cdd6e5d9a4d
Alessandro <alessandrotiberi2002@gmail.com>
2024-09-23 13:56:45 +0200

    update

---
+++ crud/client.py
@@ -0,0 +1,100 @@
+import requests,json
+from requests.auth import HTTPBasicAuth
+
+base_api_url = "https://127.0.0.1:8080/"
+username = ""
+password = ""
+
+
+def StampaMenuOperazioni():
+    print("1. Inserisci cittadino")
+    print("2. Leggi dati cittadino")
+    print("3. Modifica cittadino")
+    print("4. Elimina cittadino")
+    print("5. Inserisci credenziali")
+    print("6. Exit")
+    comando = input("Inserisci operazione: ")
+    return comando
+    
+def print_dictionary(dData):
+    for keys, values in dData.items():
+        print(keys + " - " + values)
+        
+def create_dati_cittadino():
+    nome = input("Inserisci nome: ")
+    cognome = input("Inserisci cognome: ")
+    dataN = input("Inserisci data nascita(dd/mm/yyyy): ")
+    codF = input("Inserisci codice fiscale: ")
+    datiCittadino = {"nome":nome, "cognome": cognome, "dataNascita":dataN, "codFiscale":codF}
+    return datiCittadino
+
+#Read
+def read_cittadini():
+     global base_api_url
+    
+#Update
+def update_cittadini():
+    entry_id = int(input("Enter the ID of the entry to update: "))
+    updated_data = {
+        "name": input("Enter updated name: "),
+        "surname": input("Enter updated surname: "),
+        "birthdate": input("Enter updated birthdate (YYYY-MM-DD): ")
+    }
+    
+
+#Delete
+def delete_cittadini():
+    entry_id = int(input("Enter the ID of the entry to delete: "))
+
+def AcquisisciCredenziali():
+    global username,password
+    username = input("Inserisci username: ")
+    password = input("Inserisci password: ")
+
+
+while True:
+    print("Cosa vuoi fare?")
+    comando = StampaMenuOperazioni()
+    print("Comando inserito: " + comando)
+    if comando=="1":
+        api_url = base_api_url + "add_cittadino"
+        jsonDataRequest = create_dati_cittadino()
+        response = requests.post(api_url,json=jsonDataRequest,verify=False,auth=HTTPBasicAuth(username,password))
+        print(response.json())
+        print(response.status_code)
+        print(response.headers["Content-Type"])
+        data1 = response.json()
+        if (type(response.json()) is dict):
+                print_dictionary(response.json())
+    if comando=="2":
+        api_url = base_api_url + "add_cittadino"
+        jsonDataRequest = read_cittadini()
+        response = requests.post(api_url,json=jsonDataRequest,verify=False,auth=HTTPBasicAuth(username,password))
+        print(response.json())
+        print(response.status_code)
+        print(response.headers["Content-Type"])
+        data1 = response.json()
+        if (type(response.json()) is dict):
+                print_dictionary(response.json())
+    if comando=="3":
+        api_url = base_api_url + "add_cittadino"
+        jsonDataRequest = update_cittadini()
+        response = requests.post(api_url,json=jsonDataRequest,verify=False,auth=HTTPBasicAuth(username,password))
+        print(response.json())
+        print(response.status_code)
+        print(response.headers["Content-Type"])
+        data1 = response.json()
+        if (type(response.json()) is dict):
+                print_dictionary(response.json())
+    if comando=="4":
+        api_url = base_api_url + "add_cittadino"
+        jsonDataRequest = delete_cittadini()
+        response = requests.post(api_url,json=jsonDataRequest,verify=False,auth=HTTPBasicAuth(username,password))
+        print(response.json())
+        print(response.status_code)
+        print(response.headers["Content-Type"])
+        data1 = response.json()
+        if (type(response.json()) is dict):
+                print_dictionary(response.json())            
+    if comando == "5":
+        AcquisisciCredenziali()
